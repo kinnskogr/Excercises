7661c319b53e744b6259e0d2b8c0d11c6fc212fa
kinnskogr <emanuel.strauss@gmail.com>
2013-07-27 21:34:51 -0700

    Write-only thread-safe hash map implementation

---
+++ ThreadsafeHashmap/example.cxx
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "hashmap.h"
+
+#include <list>
+
+HashMapWSafe<int, int> local_hashmap;
+
+void *modify_hashmap_1( void *ptr );
+void *modify_hashmap_2( void *ptr );
+
+main()
+{
+  
+  local_hashmap.put(1, 1);
+  local_hashmap.put(3, 1);
+  local_hashmap.put(9, 1);
+
+  std::list<int> keys = local_hashmap.keys();
+
+  printf("Original Keys:\n  %s\n", local_hashmap.print().c_str());
+
+  pthread_t thread1, thread2;
+  const char *message1 = "Thread 1";
+  const char *message2 = "Thread 2";
+  int  iret1, iret2;
+  /* Create independent threads each of which will execute function */
+  iret1 = pthread_create( &thread1, NULL, modify_hashmap_1, (void*) NULL);
+  iret2 = pthread_create( &thread2, NULL, modify_hashmap_2, (void*) NULL);
+  /* Wait till threads are complete before main continues. Unless we  */
+  /* wait we run the risk of executing an exit which will terminate   */
+  /* the process and all threads before the threads have completed.   */
+  pthread_join( thread1, NULL);
+  pthread_join( thread2, NULL);
+  printf("Thread 1 returns: %d\n",iret1);
+  printf("Thread 2 returns: %d\n",iret2);
+
+  printf("new Keys:\n  %s\n", local_hashmap.print().c_str());
+  exit(0);
+  }
+
+void *modify_hashmap_1( void *ptr )
+{
+  local_hashmap.put(2,1);
+}
+
+void *modify_hashmap_2( void *ptr )
+{
+  local_hashmap.del(1);
+  local_hashmap.put(200,1);
+}
+
+
+
---
+++ ThreadsafeHashmap/hashmap.h
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <assert.h>
+#include <pthread.h>
+#include <vector>
+#include <list>
+#include <string>
+#include <sstream>
+
+//----------------------------------------------------
+// Pair-like container for keys and values
+//----------------------------------------------------
+template<typename Keys, typename Vals> class Mapped {
+ public:
+ Mapped(Keys k, Vals v) : key(k), val(v) {};
+  Keys key;
+  Vals val;
+  bool operator<(Mapped<Keys, Vals> & comp) {return key < comp.key; }
+  bool operator>(Mapped<Keys, Vals> & comp) {return key > comp.key; }
+  bool operator<=(Mapped<Keys, Vals> & comp) {return key <= comp.key; }
+  bool operator>=(Mapped<Keys, Vals> & comp) {return key >= comp.key; }
+  
+};
+
+//----------------------------------------------------
+// HashMap base class, not thread safe
+//----------------------------------------------------
+template<typename Keys, typename Vals> class HashMap {
+ public:
+  
+  HashMap(int m = 100) : mod(m) { map.resize(mod); };
+  bool put(Keys const & k, Vals const &v);           // Insert key/values (fails if key is already present)
+  bool set(Keys const & k, Vals const &v);           // Update value (fails if key is not yet present)
+  Vals get(Keys const & k);                          // Return value (fails if key is not present)
+  bool del(Keys const & k);                          // Remove key/value (fails if key is not present)
+  int  hash(Keys const & k) {return int(k) % mod; }; // Dummy hashing function
+  std::list<Keys> keys();                            // Return a list of stored keys
+  std::string print();                               // Write a string representation of the key/value pairs
+
+ protected:
+  const int mod;
+  std::vector< std::list<Mapped<Keys, Vals> > > map;
+  
+};
+
+template<typename Keys, typename Vals> bool HashMap<Keys, Vals>::put(Keys const & k, Vals const & v)  {
+  int h = hash(k);
+  for (typename std::list<Mapped<Keys, Vals> >::iterator it = map[h].begin(), it_end = map[h].end(); it != it_end; ++it) {
+    if (it->key == k)
+      assert(0);
+  }
+  map[h].push_back(Mapped<Keys, Vals>(k, v));
+  return true;
+}
+
+template<typename Keys, typename Vals> bool HashMap<Keys, Vals>::set(Keys const & k, Vals const & v)  {
+  int h = hash(k);
+  for (typename std::list<Mapped<Keys, Vals> >::iterator it = map[h].begin(), it_end = map[h].end(); it != it_end; ++it) {
+    if (it->key == k) {
+      it->val = v;
+      return true;
+    }
+  }
+  assert(0);
+}
+
+template<typename Keys, typename Vals> Vals HashMap<Keys, Vals>::get(Keys const & k)  {
+  int h = hash(k);
+  for (typename std::list<Mapped<Keys, Vals> >::iterator it = map[h].begin(), it_end = map[h].end(); it != it_end; ++it) {
+    if (it->key == k)
+      return it->val;
+  }
+  assert(0);
+}
+
+template<typename Keys, typename Vals> bool HashMap<Keys, Vals>::del(Keys const & k)  {
+  int h = hash(k);
+  for (typename std::list<Mapped<Keys, Vals> >::iterator it = map[h].begin(), it_end = map[h].end(); it != it_end; ++it) {
+    if (it->key == k) {
+      map[h].erase(it);
+      return true;
+    }
+  }
+  assert(0);
+}
+
+template<typename Keys, typename Vals> std::list<Keys> HashMap<Keys, Vals>::keys() {
+  std::list<Keys> keys;
+  for (typename std::vector< std::list<Mapped<Keys, Vals> > >::iterator v_it = map.begin(), v_it_end = map.end(); v_it != v_it_end; ++v_it) {
+    for (typename std::list<Mapped<Keys, Vals> >::iterator l_it = v_it->begin(), l_it_end = v_it->end(); l_it != l_it_end; ++l_it) {
+      keys.push_back(l_it->key);
+    }
+  }
+  return keys;
+}
+
+template<typename Keys, typename Vals> std::string HashMap<Keys, Vals>::print() {
+  std::list<Keys> all_keys = keys();
+  all_keys.sort();
+  std::stringstream ss;
+  for (typename std::list<Keys>::iterator it = all_keys.begin(), it_end = all_keys.end(); it != it_end; ++it) {
+    ss << "(" << *it << "," << get(*it) << "), ";
+  }
+
+  return ss.str();
+
+}
+
+
+//----------------------------------------------------
+// Modify the base type such that writing operations
+// are thread safe
+//----------------------------------------------------
+template<typename Keys, typename Vals> class HashMapWSafe : public HashMap<Keys, Vals> {
+ public:
+  HashMapWSafe(int m = 100) : HashMap<Keys,Vals>::HashMap(m), rw_mutex(PTHREAD_MUTEX_INITIALIZER) { };
+  bool put(Keys const & k, Vals const &v);           // Insert key/values (fails if key is already present)
+  bool set(Keys const & k, Vals const &v);           // Update value (fails if key is not yet present)
+  bool del(Keys const & k);                          // Remove key/value (fails if key is not present)
+
+ private:
+  pthread_mutex_t rw_mutex;
+
+};
+
+template<typename Keys, typename Vals> bool HashMapWSafe<Keys, Vals>::put(Keys const & k, Vals const & v)  {
+  pthread_mutex_lock(&rw_mutex);
+  bool output = HashMap<Keys,Vals>::put(k, v);
+  pthread_mutex_unlock(&rw_mutex);
+  return output;
+}
+
+template<typename Keys, typename Vals> bool HashMapWSafe<Keys, Vals>::set(Keys const & k, Vals const & v)  {
+  pthread_mutex_lock(&rw_mutex);
+  bool output = HashMap<Keys,Vals>::set(k, v);
+  pthread_mutex_unlock(&rw_mutex);
+  return output;
+}
+
+template<typename Keys, typename Vals> bool HashMapWSafe<Keys, Vals>::del(Keys const & k)  {
+  pthread_mutex_lock(&rw_mutex);
+  bool output = HashMap<Keys,Vals>::del(k);
+  pthread_mutex_unlock(&rw_mutex);
+  return output;
+}
